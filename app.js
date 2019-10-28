b48895aff489315867cc00bebeec617bbd6223c3
vitor-amartins <vmam@cin.ufpe.br>
2019-10-28 09:20:04 -0300

    Alteartion

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const app = express();
 app.get('/', (req, res) => res.send({ message: 'Hello World!'}));
 
 const port = process.env.PORT || 3456;
-
+console.log('Olha, uma alteração!');
 app.listen(port, () => console.log(`Listening on port ${port}`));
